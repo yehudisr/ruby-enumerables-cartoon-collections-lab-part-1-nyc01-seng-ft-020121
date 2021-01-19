0ca9bffe66c440feb425b7a1f407cb6debea2051
yehudis raitman <yehudisr@gmail.com>
2021-01-19 22:29:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |index, element|
-    puts "#{index + 1} #{element}"
+    puts "#{index} #{element}"
   end
 end
